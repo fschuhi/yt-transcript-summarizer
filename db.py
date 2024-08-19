23baec1ebb441821ccd9d457fe1a9bb4e41030b1
fschuhi <fschuhi@yahoo.com>
2024-08-19 16:57:05 +0200

    Trying to PR

--- db.py
+++ db.py
@@ -1,17 +1,35 @@
 from dotenv import load_dotenv
+from functools import lru_cache
+import logging
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
+# Check if we're running in a CI environment
+IN_CI = os.getenv('CI') == 'true' or os.getenv('GITHUB_ACTIONS') == 'true'
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = None
+SessionLocal = None
 
+logger.info(f"IN_CI={IN_CI}")
+if not IN_CI:
+    if DATABASE_URL is None:
+        raise ValueError("DATABASE_URL is not set (we are not running in the CI environment)")
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@lru_cache
 def get_db():
+    if IN_CI:
+        return None
     db = SessionLocal()
     try:
         yield db
--- repositories/repository_provider.py
+++ repositories/repository_provider.py
@@ -12,21 +12,26 @@ from .user_db_repository import UserDBRepository
 
 logger = logging.getLogger(__name__)
 
+# Check if we're running in a CI environment
+IN_CI = os.getenv('CI') == 'true' or os.getenv('GITHUB_ACTIONS') == 'true'
+
 
 def get_repository(db: Session = Depends(get_db)) -> IUserRepository:
-    repository_type = os.getenv("USER_REPOSITORY_TYPE", "json")
+    repository_type = "json" if IN_CI else os.getenv("USER_REPOSITORY_TYPE", "json")
     logger.info(f"Creating repository of type: {repository_type}")
 
     if repository_type == "json":
         return UserJsonRepository('users.json')
     elif repository_type == "postgres":
+        if db is None:
+            raise ValueError("Database session is None but postgres repository type is selected")
         return UserDBRepository(db)
     else:
         raise ValueError(f"Invalid USER_REPOSITORY_TYPE: {repository_type}")
 
 
 def get_repository_provider() -> Callable[..., IUserRepository]:
-    repository_type = os.getenv("USER_REPOSITORY_TYPE", "json")
+    repository_type = "json" if IN_CI else os.getenv("USER_REPOSITORY_TYPE", "json")
     
     if repository_type == "json":
         return lambda: UserJsonRepository('users.json')
