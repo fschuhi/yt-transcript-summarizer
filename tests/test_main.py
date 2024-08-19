70fc573955dc181e29f35e19b3e9b442939c5f33
fschuhi <fschuhi@yahoo.com>
2024-08-19 11:50:08 +0200

    Added services for json and postgres, now refactoring.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,41 +1,14 @@
-import os
-# noinspection PyUnresolvedReferences
 import pytest
 from unittest.mock import patch, MagicMock
 from .conftest import mock_openai_summary, client
-# noinspection PyPackageRequirements
-from repositories.user_json_repository import UserJsonRepository
-from repositories.user_db_repository import UserDBRepository
 import logging
 
-from .test_utils import mock_repo_and_auth_service, mock_auth_and_get_token
+from .test_utils import mock_repo_and_auth_service, mocked_client_post
 
 logger = logging.getLogger(__name__)
 
 
-def test_repository_type(client):
-    repository_type = os.getenv("USER_REPOSITORY_TYPE", "json")
-
-    # Create an instance of the appropriate repository type
-    if repository_type == "postgres":
-        expected_repo = UserDBRepository(MagicMock())  # Pass a mock Session
-    else:
-        expected_repo = UserJsonRepository('test_users.json')
-
-    with patch('main.get_repository', return_value=expected_repo):
-        response = client.get("/health")
-        assert response.status_code == 200
-
-    # Verify the type of repository used
-    if repository_type == "postgres":
-        assert isinstance(expected_repo, UserDBRepository), f"Expected UserDBRepository, got {type(expected_repo)}"
-    else:
-        assert isinstance(expected_repo, UserJsonRepository), f"Expected UserJsonRepository, got {type(expected_repo)}"
-
-    logger.info(f"Confirmed repository type: {type(expected_repo).__name__}")
-
-
-def test_health_check(client):
+def test_health_endpoint(client):
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
@@ -50,79 +23,61 @@ def test_root_endpoint(client):
     assert response.json() == {"detail": "Access to this endpoint is forbidden"}
 
 
-def test_register(client):
+def test_register_endpoint(client):
     user_data = {
         "username": "newuser",
         "email": "newuser@example.com",
         "password": "password123"
     }
 
-    mock_repo, mock_auth_service = mock_repo_and_auth_service()
+    mock_auth_service = MagicMock()
     mock_auth_service.register_user.return_value = None  # Simulate successful registration
 
-    # Patch the get_repository function to return our mock repository
-    with patch('main.get_repository', return_value=mock_repo):
-        # Patch the UserAuthService to return our mock service
-        with patch('main.UserAuthService', return_value=mock_auth_service):
-            response = client.post("/register", json=user_data)
-
-    print(f"Response status code: {response.status_code}")
-    print(f"Response content: {response.content}")
+    response, response_json = mocked_client_post(client, mock_auth_service, "/register", json=user_data)
 
     assert response.status_code == 200
-    assert response.json() == {"message": "User registered successfully"}
+    assert response_json == {"message": "User registered successfully"}
     mock_auth_service.register_user.assert_called_once_with(
         user_data["username"], user_data["email"], user_data["password"]
     )
 
 
-def test_login_success(client):
+def test_token_endpoint_login_success(client):
     login_data = {
         "username": "testuser",
         "password": "password123"
     }
 
-    mock_repo, mock_auth_service = mock_repo_and_auth_service()
-    token = mock_auth_and_get_token(mock_auth_service)
+    mock_auth_service = MagicMock()
 
-    with patch('main.get_repository', return_value=mock_repo):
-        with patch('main.UserAuthService', return_value=mock_auth_service):
-            response = client.post("/token", data=login_data)
-
-    print(f"Response status code: {response.status_code}")
-    print(f"Response content: {response.content}")
+    response, response_json = mocked_client_post(client, mock_auth_service, "/token", data=login_data)
 
     assert response.status_code == 200
     assert "access_token" in response.json()
-    assert response.json()["token_type"] == "bearer"
+    assert response_json["token_type"] == "bearer"
     mock_auth_service.authenticate_user.assert_called_once_with(login_data["username"], login_data["password"])
     mock_auth_service.generate_token.assert_called_once()
 
 
-def test_login_failure(client):
+def test_token_endpoint_login_failure(client):
     login_data = {
         "username": "testuser",
         "password": "wrongpassword"
     }
 
-    mock_repo, mock_auth_service = mock_repo_and_auth_service()
+    mock_auth_service = MagicMock()
     mock_auth_service.authenticate_user.return_value = None  # Simulate failed authentication
 
-    with patch('main.get_repository', return_value=mock_repo):
-        with patch('main.UserAuthService', return_value=mock_auth_service):
-            response = client.post("/token", data=login_data)
-
-    print(f"Response status code: {response.status_code}")
-    print(f"Response content: {response.content}")
+    response, response_json = mocked_client_post(client, mock_auth_service, "/token", data=login_data)
 
     assert response.status_code == 401
-    assert response.json()["detail"] == "Incorrect username or password"
+    assert response_json["detail"] == "Incorrect username or password"
     mock_auth_service.authenticate_user.assert_called_once_with(login_data["username"], login_data["password"])
 
 
 @patch('main.get_youtube_data')
 @patch('main.openai_utils.summarize_text')
-def test_summarize_endpoint(mock_summarize_text, mock_get_youtube_data, client, mock_openai_summary, mock_youtube_data):
+def test_summarize_endpoint_authorized(mock_summarize_text, mock_get_youtube_data, client, mock_openai_summary, mock_youtube_data):
     # Setup mock data
     mock_get_youtube_data.return_value = mock_youtube_data
     mock_summarize_text.return_value = mock_openai_summary
@@ -173,7 +128,7 @@ def test_summarize_endpoint(mock_summarize_text, mock_get_youtube_data, client, 
     )
 
 
-def test_summarize_unauthorized(client):
+def test_summarize_endpoint_unauthorized(client):
     test_data = {
         "video_url": "https://www.youtube.com/watch?v=py5byOOHZM8",
         "summary_length": 300,
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,7 @@
-# tests/test_utils.py
-
 from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
-from main import app
+
+from .conftest import client
 
 
 def mock_repo_and_auth_service(repo_return_value=None, auth_service_return_value=None):
@@ -24,14 +23,12 @@ def mock_repo_and_auth_service(repo_return_value=None, auth_service_return_value
     return mock_repo, mock_auth_service
 
 
-def mock_auth_and_get_token(mock_auth_service):
-    """
-    Mock authentication and token generation.
-
-    :param mock_auth_service: Mocked authentication service
-    :return: Mocked token
-    """
-    mock_user = MagicMock()
-    mock_auth_service.authenticate_user.return_value = mock_user
-    mock_auth_service.generate_token.return_value = "dummy_token"
-    return "dummy_token"
+def mocked_client_post(client: TestClient, mock_auth_service, endpoint: str, data=None, json=None, headers=None):
+    mock_repo = MagicMock()
+    with patch('main.get_repository', return_value=mock_repo):
+        # Patch the UserAuthService to return our mock service
+        with patch('main.UserAuthService', return_value=mock_auth_service):
+            response = client.post(endpoint, json=json, data=data, headers=headers)
+    print(f"Response status code: {response.status_code}")
+    print(f"Response content: {response.content}")
+    return response, response.json()
